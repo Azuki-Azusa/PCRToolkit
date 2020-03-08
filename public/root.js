8cf1d9d0262b3a90f8d2266a6ad29727073893e3
Azuki-Azusa <cai980129@vip.qq.com>
2020-03-08 13:46:21 +0800

    comphtml

--- public/root.js
+++ public/root.js
@@ -1,3 +1,34 @@
+var root = 'http://localhost/'
+// var root = 'http://34.97.5.31/';
 // var host = 'http://yapi.demo.qunar.com/mock/86261';
-// var host = 'http://localhost/api';
-var host = 'http://34.97.5.31/api';
+var host = root + 'api';
+
+Vue.component('navbar', {
+    data: function () {
+        return {
+            count: 0,
+            verifyPath: root,
+            referencePath: root + 'kuran/reference/',
+            calculatorPath : root + 'kuran/calculator/',
+            href: window.location.href
+        }
+    },
+    template: `
+        <ul class="nav navbar-nav">
+            <li v-bind:class="{ active: verifyPath == href }"><a v-bind:href="verifyPath">验证</a></li>
+            <li v-bind:class="{ active: referencePath == href || calculatorPath == href}" class="dropdown">
+                <a v-bind:href="referencePath" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                    aria-haspopup="true" aria-expanded="false">会战<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li class="dropdown-header">作业</li>
+                    <li v-bind:class="{ active: referencePath == href }"><a v-bind:href="referencePath">会战作业</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li class="dropdown-header">计算工具</li>
+                    <li v-bind:class="{ active: calculatorPath == href }"><a v-bind:href="calculatorPath">尾刀计算</a></li>
+                </ul>
+            </li>
+        </ul>
+    `
+})
+
+new Vue({ el: 'navbar' });
